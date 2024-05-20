d938f56c5efeacc5e09a44f55c66c7adec7e8831
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-05-21 01:57:58 +0200

    create a package and use classes from other packages

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Date;
+import java.sql.*;
+import ocp11.basics.Animal;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Random r = new Random();
         System.out.println(r.nextInt(10));
+
+        Date date = new Date();
+
+        Animal animal = new Animal();
+        animal.setName("Fluffy");
+        System.out.println(animal.getName());
     }
 }
